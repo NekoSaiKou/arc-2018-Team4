ae47fbef9e486a26d5b7a74d8bce1d04e2046e7d
NekoSaiKou <ricky0711.yang@gmail.com>
2018-06-22 23:40:35 +0800

    upload linkit 7688 DUO code

---
+++ src/linkit7688/linkit_mcu/linkit_mcu.ino
@@ -0,0 +1,113 @@
+#include <SoftwareSerial.h>
+#define book_ID_length 2
+#define extension_bit 2
+#define User_ID_length 5
+#define command_length 9
+#define end_of_cmd_index command_length - 1
+
+#define Search 'A'
+#define Borrow 'B'
+#define GiveBack 'C'
+#define New_Reservation 'D'
+#define Cancel_Reservation 'E'
+
+#define E_ARC_OK "M11Z"    /*!< ok */
+#define E_ARC_Trans "M22Z" /*!< invalid cmd */
+#define E_MPU_OK "M33Z"    /*!< ok */
+#define E_MPU_Trans "M44Z" /*!< invalid cmd */
+#define E_MPU_NoRes "M55Z"
+
+int LED = 13;
+const byte rxPin = 10;
+const byte txPin = 11;
+
+// set up a new serial object
+SoftwareSerial mySerial(rxPin, txPin);
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial1.begin(57600); // open internal serial connection to MT7688AN
+  pinMode(rxPin, INPUT);
+  pinMode(txPin, OUTPUT);
+  mySerial.begin(9600);
+  pinMode(LED, OUTPUT);
+  //mySerial.listen();
+}
+void loop()
+{
+  if (mySerial.available() == command_length)
+  {
+    /*Read Data from ARC EMSK*/
+    int strlength = mySerial.available();
+    Serial.println(strlength);
+    char uart_text[command_length];
+    for (int i = 0; i < command_length; i++)
+    {
+      uart_text[i] = ' ';
+    }
+    for (int index = 0; index < command_length; index++)
+    {
+      uart_text[index] = mySerial.read();
+    }
+    /*Verify the gotten*/
+    if (uart_text[end_of_cmd_index] == 'Z')
+    {
+      switch (uart_text[0])
+      {
+      case Search:
+      case GiveBack:
+      case Borrow:
+      case New_Reservation:
+      case Cancel_Reservation:
+      {
+        Serial.println(uart_text);
+        /*send data to linkit 7688 MPU*/
+        Serial1.write(uart_text, command_length);
+        /*Wait and GET RESPONSE FORM LINKIT 7688 MPU*/
+        char rev[6] = "     ";
+        int count = 0;
+        unsigned long current_time = millis();
+        while (millis() - current_time < 5000)
+        {
+          if (Serial1.available())
+          {
+            delay(10);
+            /*Z is end of data from mySQL*/
+            rev[count] = Serial1.read();
+            if (rev[0] == '0')
+            {
+              count = -1; //indicateend of transmissting
+              Serial.println(rev);
+              delay(10);
+              mySerial.write(E_MPU_Trans);
+              break;
+            }
+            if (rev[count] == 'Z')
+            {
+              Serial1.flush();
+              count = -1; //indicateend of transmissting
+              delay(10);
+              mySerial.write(rev);
+              Serial.println(rev);
+              break;
+            }
+            count++;
+          }
+        }
+        if (count != -1)
+        {
+          delay(10);
+          mySerial.write(E_MPU_NoRes);
+        }
+        break;
+      }
+      }
+    }
+    else
+    {
+      mySerial.write(E_ARC_Trans);
+      mySerial.flush();
+    }
+  }
+}
